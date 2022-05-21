f074925ee1fad7d364791c81288ad0ff9cbb946e
KatharinaPreissinger <105777968+KatharinaPreissinger@users.noreply.github.com>
2022-05-21 23:09:43 +0200

    Update module.py

--- SampleModule/module.py
+++ SampleModule/module.py
@@ -17,6 +17,31 @@ def fun_power(x,y):
 
     return x**y
 
+def al_ratio(contour_length, contour_area):
+    """Determines ratio of contour length and area
+    
+    Parameters
+    ----------
+    contour_length : ndarray
+        1 dimensional array with type float that contains length of contours        
+    contour_area : ndarray
+        1 dimensional array with type float that contains area of contours
+    Returns
+    -------
+    array
+        ratio : an array with type float that contains ratio of contour length to area
+    
+    """
+    ratio = []
+    i = 0
+    for a in contour_area:
+        if a == 0:
+            ratio.append(0)
+        else:
+            ratio.append(contour_length[i]/a)
+        i += 1
+    return ratio
+
 class class_power():
     """A class that performs the power function."""
     def __init__(self, x, y):
