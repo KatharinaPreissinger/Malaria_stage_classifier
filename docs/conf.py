9f0b88cdf1099aba02587a7980072a89d293d167
KatharinaPreissinger <105777968+KatharinaPreissinger@users.noreply.github.com>
2022-05-21 19:30:15 +0200

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -12,10 +12,10 @@
 #
 import os
 import sys
-sys.path.append(os.path.abspath('../..'))
-sys.path.append(os.path.abspath('..'))
-#sys.path.insert(0, os.path.abspath('.'))
-import sys
+#sys.path.append(os.path.abspath('../..'))
+#sys.path.append(os.path.abspath('..'))
+sys.path.insert(0, os.path.abspath('.'))
+#import sys
 import SampleModule
 from sphinx_gallery.sorting import FileNameSortKey
 
